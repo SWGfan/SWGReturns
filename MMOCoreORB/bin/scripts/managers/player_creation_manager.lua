c591d07e97f6ebe0aa61368ba366d002214d9b45
ben <dfitz@me.com>
2025-09-15 08:54:02 -0400

    Update player_creation_manager.lua

--- MMOCoreORB/bin/scripts/managers/player_creation_manager.lua
+++ MMOCoreORB/bin/scripts/managers/player_creation_manager.lua
@@ -1,10 +1,10 @@
 --Should all created players start with God Mode? 1 = yes, 0 = no
 freeGodMode = 0;
 --How many cash credits new characters start with after creating a character (changed during test phase, normal value is 100)
-startingCash = 200000
+startingCash = 20000
 --startingCash = 100000
 --How many bank credits new characters start with after creating a character (changed during test phase, normal value is 1000)
-startingBank = 200000
+startingBank = 20000
 --startingBank = 100000
 --How many skill points a new characters start with
 skillPoints = 500
@@ -13,10 +13,10 @@ professions = {
 	"combat_brawler",
 	"combat_marksman",
 	"crafting_artisan",
+	"combat_jedi",
 	"outdoors_scout",
 	"science_medic",
-	"social_entertainer",
-	--"jedi_padawan"
+	"social_entertainer"
 }
 
 marksmanPistol = "object/weapon/ranged/pistol/pistol_cdef.iff"
@@ -25,11 +25,13 @@ marksmanRifle = "object/weapon/ranged/rifle/rifle_cdef.iff"
 
 marksmanCarbine = "object/weapon/ranged/carbine/carbine_cdef.iff"
 
-brawlerOneHander = "object/weapon/melee/knife/knife_survival.iff"
+brawlerOneHander = "object/weapon/melee/knife/knife_stone.iff"
 
 brawlerTwoHander = "object/weapon/melee/axe/axe_heavy_duty.iff"
 
 brawlerPolearm = "object/weapon/melee/polearm/lance_staff_wood_s1.iff"
+
+survivalKnife = "object/weapon/melee/knife/knife_survival.iff"
 
 genericTool = "object/tangible/crafting/station/generic_tool.iff"
 
@@ -41,9 +43,13 @@ chemicalTool = "object/tangible/survey_tool/survey_tool_liquid.iff"
 
 slitherhorn = "object/tangible/instrument/slitherhorn.iff"
 
-Swoop_Bike = "object/tangible/deed/vehicle_deed/speederbike_swoop_deed.iff"
+marojMelon = "object/tangible/food/foraged/foraged_fruit_s1.iff"
+
+SwoopBike = "object/tangible/deed/vehicle_deed/speederbike_swoop_deed.iff"
 
 robe = "object/tangible/wearables/robe/robe_jedi_padawan.iff"
+
+saber = "object/weapon/melee/sword/crafted_saber/sword_lightsaber_s9_training.iff"
 
 jediTool = "object/tangible/crafting/station/jedi_tool.iff"
 
@@ -51,14 +57,16 @@ jediCrystal = "object/tangible/component/weapon/lightsaber/lightsaber_module_for
 
 gasTool = "object/tangible/survey_tool/survey_tool_gas.iff"
 
+stim = "object/tangible/medicine/crafted/crafted_stimpack_sm_s1_a.iff"
+
 professionSpecificItems = {
 	combat_brawler = { brawlerOneHander, brawlerTwoHander, brawlerPolearm },
 	combat_marksman = { marksmanPistol, marksmanCarbine, marksmanRifle },
 	crafting_artisan = { genericTool, mineralTool, chemicalTool },
-	--jedi_padawan = { robe, jediTool, jediCrystal, genericTool, mineralTool, chemicalTool, gasTool },
+  	combat_jedi = { genericTool, mineralTool, chemicalTool, gasTool, jediTool, jediCrystal, stim, saber },
 	outdoors_scout = { genericTool },
-	science_medic = { foodTool },
+	science_medic = { foodTool, chemicalTool, mineralTool },
 	social_entertainer = { slitherhorn }
 }
 
-commonStartingItems = { Swoop_Bike }
+commonStartingItems = { marojMelon, survivalKnife, SwoopBike }
