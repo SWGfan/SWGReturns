c2d5c9e4c54fc811535c951bf6135a5ab9909dad
ben <dfitz@me.com>
2025-09-15 08:03:05 -0400

    Update SkillManager.h

--- MMOCoreORB/src/server/zone/managers/skill/SkillManager.h
+++ MMOCoreORB/src/server/zone/managers/skill/SkillManager.h
@@ -1,6 +1,6 @@
 /*
-				Copyright <SWGEmu>
-		See file COPYING for copying conditions.
+                Copyright <SWGEmu>
+        See file COPYING for copying conditions.
 */
 
 #ifndef SKILLMANAGER_H_
@@ -15,7 +15,7 @@ namespace server {
 namespace zone {
 namespace objects {
 namespace creature {
-	class CreatureObject;
+    class CreatureObject;
 }
 }
 }
@@ -27,7 +27,7 @@ namespace server {
 namespace zone {
 namespace objects {
 namespace player {
-	class PlayerObject;
+    class PlayerObject;
 }
 }
 }
@@ -41,108 +41,116 @@ namespace managers {
 namespace skill {
 
 class SkillManager : public Singleton<SkillManager>, public Logger, public Object {
-	PerformanceManager* performanceManager;
+    PerformanceManager* performanceManager;
 
-	HashTable<String, Reference<Ability*> > abilityMap;
-	HashTable<uint32, Reference<Skill*> > skillMap;
+    HashTable<String, Reference<Ability*> > abilityMap;
+    HashTable<uint32, Reference<Skill*> > skillMap;
 
-	Reference<Skill*> rootNode;
+    Reference<Skill*> rootNode;
 
-	VectorMap<String, int> defaultXpLimits;
+    VectorMap<String, int> defaultXpLimits;
 
-	VectorMap<uint32, int> droidProgramSizes;
+    // Space droid program sizes: program hash -> size
+    VectorMap<uint32, int> droidProgramSizes;
 
-	SortedVector<String> droidCommands;
+    // All known droid command names ("programName" portion, e.g. for ability "droid+<programName>")
+    SortedVector<String> droidCommands;
 
-	bool apprenticeshipEnabled;
+    bool apprenticeshipEnabled;
 
 public:
-	SkillManager();
-	~SkillManager();
+    SkillManager();
+    ~SkillManager();
 
-	static int includeFile(lua_State* L);
-	static int addSkill(lua_State* L);
+    static int includeFile(lua_State* L);
+    static int addSkill(lua_State* L);
 
-	void loadLuaConfig();
-	void loadClientData();
-	void loadFromLua();
-	void loadSkill(LuaObject* skill);
-	void loadXpLimits();
+    void loadLuaConfig();
+    void loadClientData();
+    void loadFromLua();
+    void loadSkill(LuaObject* skill);
+    void loadXpLimits();
 
-	void addAbility(PlayerObject* ghost, const String& abilityName, bool notifyClient = true);
-	void removeAbility(PlayerObject* ghost, const String& abilityName, bool notifyClient = true);
+    void addAbility(PlayerObject* ghost, const String& abilityName, bool notifyClient = true);
+    void removeAbility(PlayerObject* ghost, const String& abilityName, bool notifyClient = true);
 
-	void addAbilities(PlayerObject* ghost, const Vector<String>& abilityNames, bool notifyClient = true);
-	void removeAbilities(PlayerObject* ghost, const Vector<String>& abilityNames, bool notifyClient = true);
+    void addAbilities(PlayerObject* ghost, const Vector<String>& abilityNames, bool notifyClient = true);
+    void removeAbilities(PlayerObject* ghost, const Vector<String>& abilityNames, bool notifyClient = true);
 
-	void addDroidCommands(PlayerObject* ghost, const Vector<String>& abilityNames, bool notifyClient = true);
-	void removeDroidCommands(PlayerObject* ghost);
+    // Droid command helpers
+    void addDroidCommands(PlayerObject* ghost, const Vector<String>& abilityNames, bool notifyClient = true);
+    void removeDroidCommands(PlayerObject* ghost);
+    void getPlayerDroidCommands(PlayerObject* ghost, Vector<String>& playerDroidCommands);
 
-	bool awardSkill(const String& skillName, CreatureObject* creature, bool notifyClient = true, bool awardRequiredSkills = false, bool noXpRequired = false);
-	void awardDraftSchematics(Skill* skill, PlayerObject* ghost, bool notifyClient = true);
+    bool awardSkill(const String& skillName, CreatureObject* creature, bool notifyClient = true, bool awardRequiredSkills = false, bool noXpRequired = false);
+    void awardDraftSchematics(Skill* skill, PlayerObject* ghost, bool notifyClient = true);
 
-	bool surrenderSkill(const String& skillName, CreatureObject* creature, bool notifyClient = true, bool verifyFrs = true, bool allowPilot = false);
-	void surrenderAllSkills(CreatureObject* creature, bool notifyClient = true, bool removeForceProgression = true, bool removePilot = false);
+    bool surrenderSkill(const String& skillName, CreatureObject* creature, bool notifyClient = true, bool verifyFrs = true, bool allowPilot = false);
+    void surrenderAllSkills(CreatureObject* creature, bool notifyClient = true, bool removeForceProgression = true, bool removePilot = false);
 
-	/**
-	 * Checks if the player can learn the skill (fulfills skill prerequisites, enough skill points and enough XP).
-	 * @param skillName the name of the skill to check if the player can learn.
-	 * @param creature the player creature.
-	 * @param noXpRequired XP check is skipped if this is set to true (used for character builder terminals and
-	 * grant skill command).
-	 * @return true if the player fulfills the requirements.
-	 */
-	bool canLearnSkill(const String& skillName, CreatureObject* creature, bool noXpRequired);
+    /**
+     * Checks if the player can learn the skill (fulfills skill prerequisites, enough skill points and enough XP).
+     * @param skillName the name of the skill to check if the player can learn.
+     * @param creature the player creature.
+     * @param noXpRequired XP check is skipped if this is set to true (used for character builder terminals and
+     * grant skill command).
+     * @return true if the player fulfills the requirements.
+     */
+    bool canLearnSkill(const String& skillName, CreatureObject* creature, bool noXpRequired);
 
-	/**
-	 * Checks if the player fulfills the skill prerequisites and has enough XP for the skill.
-	 * @param skillName the name of the skill to check.
-	 * @param creature the player creature.
-	 * @return true if the player fulfills the requirements.
-	 */
-	bool fulfillsSkillPrerequisitesAndXp(const String& skillName, CreatureObject* creature);
+    /**
+     * Checks if the player fulfills the skill prerequisites and has enough XP for the skill.
+     * @param skillName the name of the skill to check.
+     * @param creature the player creature.
+     * @return true if the player fulfills the requirements.
+     */
+    bool fulfillsSkillPrerequisitesAndXp(const String& skillName, CreatureObject* creature);
 
-	/**
-	 * Checks if the player fulfills the skill prerequisites.
-	 * @param skillName the name of the skill to check.
-	 * @param creature the player creature.
-	 * @return true if the player fulfills the requirements.
-	 */
-	bool fulfillsSkillPrerequisites(const String& skillName, CreatureObject* creature);
+    /**
+     * Checks if the player fulfills the skill prerequisites.
+     * @param skillName the name of the skill to check.
+     * @param creature the player creature.
+     * @return true if the player fulfills the requirements.
+     */
+    bool fulfillsSkillPrerequisites(const String& skillName, CreatureObject* creature);
 
-	bool villageKnightPrereqsMet(CreatureObject* creature, const String& skillToDrop);
+    bool villageKnightPrereqsMet(CreatureObject* creature, const String& skillToDrop);
 
-	int getForceSensitiveSkillCount(CreatureObject* creature, bool includeNoviceMasterBoxes);
+    // Counts & helpers (used by combat_jedi / grey Jedi logic)
+    int getForceSensitiveSkillCount(CreatureObject* creature, bool includeNoviceMasterBoxes);
+    int getSpecificSkillCount(CreatureObject* creature, const String& skill);
 
-	void updateXpLimits(PlayerObject* ghost);
+    // combat_jedi / grey Jedi helpers merged from your other file
+    void awardForceFromSkills(CreatureObject* creature);
+    void awardResetSkills(CreatureObject* creature);
 
-	Skill* getSkill(const String& skillName) const {
-		return skillMap.get(skillName.hashCode()).get();
-	}
+    void updateXpLimits(PlayerObject* ghost);
 
-	Skill* getSkill(uint32 hashCode) const {
-		return skillMap.get(hashCode).get();
-	}
+    Skill* getSkill(const String& skillName) const {
+        return skillMap.get(skillName.hashCode()).get();
+    }
 
-	Ability* getAbility(const String& abilityName) const {
-		return abilityMap.get(abilityName).get();
-	}
+    Skill* getSkill(uint32 hashCode) const {
+        return skillMap.get(hashCode).get();
+    }
 
-	PerformanceManager* getPerformanceManager() {
-		return performanceManager;
-	}
+    Ability* getAbility(const String& abilityName) const {
+        return abilityMap.get(abilityName).get();
+    }
 
-	inline bool isApprenticeshipEnabled() const {
-		return apprenticeshipEnabled;
-	}
+    PerformanceManager* getPerformanceManager() {
+        return performanceManager;
+    }
 
-	void removeSkillRelatedMissions(CreatureObject* creature, Skill* skill);
+    inline bool isApprenticeshipEnabled() const {
+        return apprenticeshipEnabled;
+    }
 
-	int getDroidProgramSize(uint32 programHash) {
-		return droidProgramSizes.get(programHash);
-	}
+    void removeSkillRelatedMissions(CreatureObject* creature, Skill* skill);
 
-	void getPlayerDroidCommands(PlayerObject* ghost, Vector<String>& playerDroidCommands);
+    int getDroidProgramSize(uint32 programHash) {
+        return droidProgramSizes.get(programHash);
+    }
 };
 
 }
