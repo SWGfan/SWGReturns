e21cb7469dc036bff459e2d8904d4885415fe381
ben <dfitz@me.com>
2025-10-07 01:18:53 -0400

    Update config.lua

--- MMOCoreORB/bin/conf/config.lua
+++ MMOCoreORB/bin/conf/config.lua
@@ -153,7 +153,7 @@ Core3 = {
 	TrePath = "/home/swgadmin/tre",
 	TreFiles = {
 		"mandalore.tre",
-		"skills_01.tre",
+		"returns1.tre",
 		"default_patch.tre",
 		"patch_sku1_14_00.tre",
 		"patch_14_00.tre",
