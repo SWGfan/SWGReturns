f0fd3518b11397dfff10f09fe984526b9996dc0a
ben <dfitz@me.com>
2025-10-17 04:00:48 -0400

    Refactor RemoveDisabledInvulnerableTask constructor and run method

--- MMOCoreORB/src/server/zone/objects/ship/ai/events/RemoveDisabledInvulnerableTask.h
+++ MMOCoreORB/src/server/zone/objects/ship/ai/events/RemoveDisabledInvulnerableTask.h
@@ -7,6 +7,8 @@
 
 #include "server/zone/objects/ship/ai/ShipAiAgent.h"
 #include "templates/params/ship/ShipFlag.h"
+// Use the DestructibleBuildingDataComponent constant for INVULNERABLE
+#include "server/zone/objects/building/components/DestructibleBuildingDataComponent.h"
 
 namespace server {
 namespace zone {
@@ -16,37 +18,37 @@ namespace ai {
 namespace events {
 
 class RemoveDisabledInvulnerableTask : public Task {
-    ManagedWeakReference<ShipAiAgent*> ship;
+	ManagedWeakReference<ShipAiAgent*> ship;
 
 public:
-    RemoveDisabledInvulnerableTask(ShipAiAgent* shipO) {
-        ship = shipO;
+	RemoveDisabledInvulnerableTask(ShipAiAgent* shipO) {
+		ship = shipO;
 
-        auto zone = shipO->getZone();
+		auto zone = shipO->getZone();
 
-        if (zone != nullptr) {
-            setCustomTaskQueue(zone->getZoneName());
-        }
-    }
+		if (zone != nullptr) {
+			setCustomTaskQueue(zone->getZoneName());
+		}
+	}
 
-    void run() {
-        ManagedReference<ShipAiAgent*> strongShip = ship.get();
+	void run() {
+		ManagedReference<ShipAiAgent*> strongShip = ship.get();
 
-        if (strongShip == nullptr) {
-            return;
-        }
+		if (strongShip == nullptr) {
+			return;
+		}
 
-        Locker locker(strongShip);
+		Locker locker(strongShip);
 
-        auto zone = strongShip->getZone();
+		auto zone = strongShip->getZone();
 
-        if (zone == nullptr) {
-            return;
-        }
+		if (zone == nullptr) {
+			return;
+		}
 
-        // Core3 replacement for removed DISABLED_INVULNERABLE flag
-        strongShip->removeShipFlag(OptionBitmask::INVULNERABLE);
-    }
+		// Core3: INVULNERABLE is defined in DestructibleBuildingDataComponent
+		strongShip->removeShipFlag(DestructibleBuildingDataComponent::INVULNERABLE);
+	}
 };
 
 } // namespace events
