c736e3e9dfa606295d00b11dbc3aa1f69085c2b4
ben <dfitz@me.com>
2025-10-17 00:33:06 -0400

    Refactor PingClient to use getIPAddress method

--- MMOCoreORB/src/server/ping/PingClient.h
+++ MMOCoreORB/src/server/ping/PingClient.h
@@ -1,42 +1,58 @@
 /*
-				Copyright <SWGEmu>
-		See file COPYING for copying conditions.*/
+                Copyright <SWGEmu>
+        See file COPYING for copying conditions.
+*/
 
 #ifndef PINGCLIENT_H_
 #define PINGCLIENT_H_
 
+#include "server/network/DatagramServiceThread.h"
+#include "server/network/BaseClientProxy.h"
+#include "server/network/Socket.h"
+#include "server/network/SocketAddress.h"
 #include "engine/engine.h"
 
 class PingClient : public BaseClientProxy {
 public:
-	PingClient(DatagramServiceThread* serv, Socket* sock, SocketAddress& addr) : BaseClientProxy(sock, addr) {
-		setLoggingName("PingClient " + getFullIPAddress());
-		setLogging(false);
-		setLogLevel(Logger::FATAL);
+    PingClient(DatagramServiceThread* serv, Socket* sock, SocketAddress& addr)
+        : BaseClientProxy(sock, addr) {
+        // Use getIPAddress() instead of removed getFullIPAddress()
+        setLoggingName("PingClient " + getIPAddress());
+        setLogging(false);
+        setLogLevel(Logger::FATAL);
 
-		init(serv);
-	}
+        init(serv);
+    }
 
-	virtual ~PingClient() {
-	}
+    virtual ~PingClient() {
+    }
 
-	void disconnect(bool doLock = true) {
-		if (isDisconnected())
-			return;
+    void disconnect(bool doLock = true) {
+        if (isDisconnected())
+            return;
 
-		String time;
-		Logger::getTime(time);
+        String time;
+        Logger::getTime(time);
 
-		StringBuffer msg;
-		msg << time << " [PingServer] disconnecting client \'" << getFullIPAddress() << "\'\n";
-		Logger::console.log(msg);
+        StringBuffer msg;
+        msg << time << " [PingServer] disconnecting client '" << getIPAddress() << "'\n";
+        Logger::console.log(msg);
 
-		BaseClientProxy::disconnect(doLock);
-	}
+        BaseClientProxy::disconnect(doLock);
+    }
 
-	void sendMessage(Message* msg) {
-		BaseClientProxy::sendPacket(cast<BasePacket*>(msg));
-	}
+    void sendMessage(Message* msg) {
+        BaseClientProxy::sendPacket(cast<BasePacket*>(msg));
+    }
+
+    // Optional: accessors for convenience
+    sys::lang::String getIPAddress() const {
+        return BaseClientProxy::getIPAddress();
+    }
+
+    int getPort() const {
+        return BaseClientProxy::getPort();
+    }
 };
 
 #endif /* PINGCLIENT_H_ */
