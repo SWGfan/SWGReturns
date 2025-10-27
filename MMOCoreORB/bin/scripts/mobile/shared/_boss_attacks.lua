1a790da9ec0c04baaa23ccd2945f44ba570aca3c
swgadmin <swgadmin@localhost.localdomain>
2025-10-27 17:37:17 -0400

    added mandalore

--- MMOCoreORB/bin/scripts/mobile/shared/_boss_attacks.lua
+++
@@ -1,7 +0,0 @@
--- Consolidate strong melee/ranged templates so bosses feel distinct
-bossAttacks = {
-  lightsaber_heavy = merge(lightsabermaster, forcepowermaster, forcewielder),
-  saber_duelist    = merge(lightsabermaster, fencermaster, pikemanmaster),
-  force_controller = merge(forcepowermaster, tkmaster),
-  droid_assassin   = merge(bountyhuntermid, marksmanmaster, brawlernovice)
-}
